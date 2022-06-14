9c122aa2baeb9e636b4753f43cf1c0c10a270dac
Imran-Noland <85587020+Imran-Noland@users.noreply.github.com>
2022-06-14 16:54:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+status= "";
+word= "";
+function setup(){
+canvas = createCanvas(480, 380);
+canvas.center();
+video= createCapture(VIDEO);
+video.size(480, 380);
+video.hide();
+}
+function start(){
+objectDetector= ml5.objectDetector("cocossd", modelLoaded);
+document.getElementById("status").innerHTML= "status: Detecting Objects";
+word= document.getElementById("word").value;
+}
+function modelLoaded(){
+console.log("modelLoaded");
+status=true;
+}
+function draw(){
+image(video, 0, 0, 480, 380);
+}
