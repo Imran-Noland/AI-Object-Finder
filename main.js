7a40151a61963aa316f10f315b4453b38916d068
Imran-Noland <85587020+Imran-Noland@users.noreply.github.com>
2022-06-15 16:52:58 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 status= "";
 word= "";
+objects= [];
 function setup(){
 canvas = createCanvas(480, 380);
 canvas.center();
@@ -17,5 +18,30 @@ console.log("modelLoaded");
 status=true;
 }
 function draw(){
-image(video, 0, 0, 480, 380);
-}
+    image(video, 0, 0, 480, 380);
+    if(status != ""){
+    objectDetector.detect(video, gotResults);
+    for(i=0;i<objects.length;i++){
+    document.getElementById("status").innerHTML= "Status: Objects Detected";
+    fill("#ff0000");
+    percent=floor(objects[i].confidence*100);
+    text(objects[i].label+" " + percent + "%", objects[i].x+15, objects[i].y+15);
+    noFill();
+    stroke("#ff0000");
+    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    if(objects[i].label==word){
+        document.getElementById("number_of_objects").innerHTML= "Object Found: " + word;
+    }
+    else{
+    document.getElementById("number_of_objects").innerHTML= "Object Not Found: " + word;
+    }
+    }
+    }
+    }
+    function gotResults(error, results){
+    if(error){
+    console.log(error);
+    }
+    console.log(results);
+    objects= results;
+    }
